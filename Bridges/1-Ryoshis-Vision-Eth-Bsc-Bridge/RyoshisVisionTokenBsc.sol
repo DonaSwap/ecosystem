e2532334f1aaae460f64dfcdd677345ee7907a8b
DONASWAP <85658707+DonaSwap@users.noreply.github.com>
2022-03-26 16:08:09 +0100

    Delete RyoshisVisionTokenBsc.sol

--- Bridges/1-Ryoshis-Vision-Eth-Bsc-Bridge/RyoshisVisionTokenBsc.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.8.13;
-
-import './RyoshisVisionTokenBase.sol';
-
-contract RyoshisVisionTokenBsc is RyoshisVisionTokenBase {
-  constructor() RyoshisVisionTokenBase('BEP20 Ryoshis Vision', 'RYOSHI') {}
