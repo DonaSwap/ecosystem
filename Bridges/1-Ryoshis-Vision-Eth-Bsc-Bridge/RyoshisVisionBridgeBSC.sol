413b68c0c3b09666c96a5b82a2a2e71c0bfeadad
DONASWAP <85658707+DonaSwap@users.noreply.github.com>
2022-03-26 15:41:58 +0100

    Create RyoshisVisionBridgeBSC.sol

---
+++ Bridges/1-Ryoshis-Vision-Eth-Bsc-Bridge/RyoshisVisionBridgeBSC.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.13;
+
+import './RyoshisVisionBridgeBase.sol';
+
+contract RyoshisVisionBridgeBsc is RyoshisVisionBridgeBase {
+  constructor(address token) RyoshisVisionBridgeBase(token) {}
+}
